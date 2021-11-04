d3001b255ba636c750eaf6df5c0fb7ee971f82b6
ikitsuchi <ikitsuchi@outlook.com>
2021-11-04 18:22:38 +0800

    Add: xargs.c

---
+++ Task_2/src/xargs.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    fprintf(stderr, "xargs: Error.\n");
+    return 0;
+  }
+  char *buffer = (char *)malloc(sizeof(char) * 114514);
+  int stdin_length = read(STDIN_FILENO, buffer, 114514);
+
+  char *exec_argv[4096];
+  int exec_argc = 1; 
+  exec_argv[0] = (char *) malloc(sizeof(char) * (strlen(argv[1])));
+  strcpy(exec_argv[0], argv[1]);
+  for (int i = 0, j; i < stdin_length; ++i) {
+    if (buffer[i] == ' ' || !isprint(buffer[i])) continue;
+    for (j = i; buffer[j] != ' ' && isprint(buffer[j]) && j < stdin_length; ++j);
+    exec_argv[exec_argc] = (char *) malloc(sizeof(char) * (j - i + 1));
+    strncpy(exec_argv[exec_argc], buffer + i, j - i);
+    exec_argv[exec_argc][j - i] = '\0';
+    ++exec_argc;
+    i = j;
+  }
+  for (int i = 2; i < argc; ++i) {
+    exec_argv[exec_argc + i - 2] = (char *) malloc(sizeof(char) * strlen(argv[i]));
+    strcpy(exec_argv[exec_argc + i - 2], argv[i]);
+    exec_argv[exec_argc][strlen(argv[i])] = '\0';
+    ++exec_argc;
+  }
+
+  execve(exec_argv[0], exec_argv, NULL);
+  free(buffer);
+  return 0;
+}
