482cc016602a144fa32e74e5a525770368240ab8
ikitsuchi <ikitsuchi@outlook.com>
2021-11-12 10:54:57 +0800

    Fix bugs

--- Task_2/src/cat.c
+++ Task_2/src/cat.c
@@ -1,31 +1,38 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+
+#include "../incl/error.h"
 
 int main(int argc, char *argv[]) {
   char *buffer = (char *)malloc(114514);
   int length;
   if (argc == 1) {
     while ((length = read(STDIN_FILENO, buffer, 114514)) > 0)
-      fwrite(buffer, length, 1, stdout);
+      write(STDOUT_FILENO, buffer, length);
     return 0;
   }
+  int return_value = 0;
   for (int i = 1; i < argc; ++i) {
     if (strcmp(argv[i], "-") == 0) {
-      while (read(STDIN_FILENO, buffer, 114514) > 0)
-        fwrite(buffer, length, 1, stdout);
+      while ((length = read(STDIN_FILENO, buffer, 114514)) > 0)
+        write(STDOUT_FILENO, buffer, length);
     } else {
-      FILE *fp = fopen(argv[i], "r");
-      if (fp != NULL) {
-        fread(buffer, 114514, 1, fp);
-        fwrite(buffer, 114514, 1, stdout);
-        fclose(fp);
+      int fp = open(argv[i], O_RDONLY);
+      if (fp >= 0) {
+        while ((length = read(fp, buffer, 114514)) > 0)
+          write(STDOUT_FILENO, buffer, length);
+        close(fp);
       } else {
-        fprintf(stderr, "cat: %s: That file or directory doesn't exist.\n", argv[i]);
+        printError("cat", argv[i], errno);
+        return_value = -1;
       }
     }
   }
   free(buffer);
-  return 0;
+  return return_value;
 }
